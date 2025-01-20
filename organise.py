2466b9f59576e11c916907322b20f289c5e27265
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-01-20 21:06:59 +0000

    Finished working code

--- organise.py
+++ organise.py
@@ -1,7 +1,8 @@
 import os
+import shutil
 
 
-directory = '/Users/fola/Documents'
+directory = '/Users/fola/Desktop'
 
 
 folders = {
@@ -14,33 +15,42 @@ folders = {
    "Archives": [".zip", ".rar"]
 }
 
-
+#Check to see if the directory doesn't exist before 
 if not os.path.exists(directory):
    print ("This path does not exist")
 
 else:
+   #If it is a directory list the files 
    if os.path.isdir(directory):
       print("Directory found. Proceeding to list files ")
       files = os.listdir(directory)
 
-      if not files == 0 :
+      #If there are no files to sort in the directory 
+      if not files :
          print("Directory is empty!")
       else:
-            
-         #Creating folders
+         #If there are files to sort in the directory. 
+         #We create folders 
          for folder in folders: 
-            folder_path = os.path.join('/Users/fola/Documents', folder)
+            folder_path = os.path.join(directory, folder)
             if not os.path.exists(folder_path):
                os.mkdir(folder_path)
 
+         #Check the files in our directory
+         #We split the text and filename wwe get the filename and we get the extension 
          for file in files:
-            root, extension = os.path.splitext(file)
+            name, extension = os.path.splitext(file)
             extension.lower()
 
+            #We get the key-value pair from the folders dictionary
+            #the keys are stored in folder, and the list of formats are stored in format_list
             for folder, format_list in folders.items() : 
                for format in format_list:
-                  if extension == format:
-                     if os.path.exists(directory + '/folder'):
+                  if extension.lower() == format:
+                     destination_folder = os.path.join(directory, folder)
+                     if os.path.exists(destination_folder):
+                        shutil.move(os.path.join(directory, file), destination_folder)
+
 
 
 
