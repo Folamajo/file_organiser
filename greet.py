19ecee721e45b66db68badb4a5a9353a563549c6
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-01-20 19:31:06 +0000

    updated file organiser

---
+++ greet.py
@@ -0,0 +1,7 @@
+def greet():
+   print("Hello world")
+
+print("This will always run")
+
+if __name__ == "__main__":
+   greet()
--- organise.py
+++ organise.py
@@ -1,61 +1,61 @@
 import os
-import shutil
 
-folder_to_organise = "C:/Users/Fola/Downloads"
 
-#create a list of all the files within the folder we want to organise 
-all_files = os.listdir(folder_to_organise)
+directory = '/Users/fola/Documents'
 
-#Created folder names to sort out files
-folder_names = {
-   'Documents' : ['pdf', 'html', 'txt', 'docx', 'xlsx' ],
-   'Images' : ['png', 'jpeg', 'gif', 'jpg'],
-   'Videos' : ['mp4', 'mov', 'avi'],
-   'Archives' : ['zip', 'rar', 'tar'],
-   'Other': []
+
+folders = {
+   "Images" : [".jpg", ".png", ".gif", ".svg", ".heif"],
+   "Documents" : [".txt", ".pdf", ".doc", ".docx", ".ppt", ".pptx", ".csv"],
+   "Video" : [".mp4", ".mov", ".avi"],
+   "Audio" : [".mp3", ".wav"],
+   "Spreadsheets" : [".xls", ".xlsx", ".csv"],
+   "Code" : [".js", ".html", ".py", ".css", ".java"],
+   "Archives": [".zip", ".rar"]
 }
 
-#Function to organise files
 
-def organise_files(directory):
-   #Create folders that will store our files
-   for folder in folder_names.keys():
-      folder_path = os.path.join(directory, folder)
-      if not os.path.exists(folder_path):
-         os.makedirs(folder_path)
+if not os.path.exists(directory):
+   print ("This path does not exist")
 
-   #Check if the folder exists
-   
-      #If it doesn't exist:
-         #display an error and stop the program
-      if not os.path.isdir(directory):
-         print("Error: Folder does not exist")
-         exit()
-      #If it exists:
+else:
+   if os.path.isdir(directory):
+      print("Directory found. Proceeding to list files ")
+      files = os.listdir(directory)
+
+      if not files == 0 :
+         print("Directory is empty!")
       else:
-        
-         #If it has no files:
-            #display a message and stop 
-         if len(os.listdir(directory)) == 0:
-            print("No files found in the directory")
-            exit()
-         #If it has files
-         else:
             
+         #Creating folders
+         for folder in folders: 
+            folder_path = os.path.join('/Users/fola/Documents', folder)
+            if not os.path.exists(folder_path):
+               os.mkdir(folder_path)
 
-         
-            #Loop through our files 
-            for file in os.listdir(directory):
-               #For each file:
-                  #Check if its extension matches any values in our folder names values
-                     #If it matches:
-                        #Check if a folder for that category exists 
-                           #if folder exists:
-                              #Move File into that folder
-                           #Else if it doesn't
-                              #Create that folder 
-                              #Move the file into that folder 
-                  #If the the file does not match any extensions: 
-                     #Print a message that skips the file 
-      #Log the results
-      #Print "File organisation completed"
+         for file in files:
+            root, extension = os.path.splitext(file)
+            extension.lower()
+
+            for folder, format_list in folders.items() : 
+               for format in format_list:
+                  if extension == format:
+                     if os.path.exists(directory + '/folder'):
+
+
+
+
+
+
+# checks if path exists 
+# if os.path.exists(directory):
+#    print("this path exists")
+#    print(os.listdir(directory))
+# else:
+#    print("this path does not exist")
+
+# os.listdir(directory)
+
+# folders = {
+#    "Images", "Documents", "Video", "Audio", "Spreadsheets", "Code", "Archives", "Uncategorised"
+# }
